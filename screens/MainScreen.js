7016968613b7e2efda82b8043e1cc46ed8bc3812
ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-11-08 17:50:11 +0100

    main screen makes get all user gata

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView, View, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import styles from '../styles/MainScreen';
 import LessonsList from '../components/LessonsList';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getUSerProfileByToken } from '../services/UserService';
 const MainScreen = () => {
   const navigation = useNavigation();
+
+  useEffect(() =>{
+    const fetchUserData = async () => {
+      try {
+        const responseuserData = await getUSerProfileByToken();
+        await AsyncStorage.setItem('userData', JSON.stringify(responseuserData))
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    };
+    fetchUserData();
+  }, []);
   
   return (
     <ScrollView style={styles.container} contentContainerStyle={{ flexGrow: 1 }}>
